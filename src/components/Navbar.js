f9a1ca57b001adb180b0711b170df067047cdb30
dzendzen <dzen@hotmail.fr>
2019-10-08 17:10:34 +0200

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import  './navbar.css';
+function Navbar (){
+    return(
+    <div>
+        <nav className= "Navbar"><b>Simpsons</b>best quotes</nav>
+    </div>
+    );
+}
+
+export default Navbar
